e2706bbfb4fc9ed585c9efc56b1f9f1b0daf866b
Ruan <domruan333@gmail.com>
2023-12-01 15:56:03 -0300

    Adicionado classes para desconto e produto

--- EletronicStoreManager/Entities/Discount.cs
+++ EletronicStoreManager/Entities/Discount.cs
@@ -8,5 +8,24 @@ namespace EletronicStoreManager.Entities
 {
     internal class Discount
     {
+        private int IdUp {  get; set; }
+        private int IdDiscount { get; set; }
+        private string DiscountType { get; set; }
+        //private Product product { get; set; }
+        private DateTime BeginDate { get; set; }
+        private DateTime EndDate { get; set; }
+
+        public Discount(string discountType, DateTime beginDate, DateTime endDate)
+        {   
+            IdDiscount = 1 + IdUp++;
+            DiscountType = discountType;
+            BeginDate = beginDate;
+            EndDate = endDate;
+        }
+
+        public override string ToString()
+        {
+            return "[ID do Desconto: " + IdDiscount + " | Tipo do Desconto: " + DiscountType + " | Data de Inicio : " + BeginDate + " | Data de Fim: " + EndDate + "]"; 
+         }
     }
 }
--- EletronicStoreManager/Entities/Item.cs
+++ EletronicStoreManager/Entities/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,47 @@ namespace EletronicStoreManager.Entities
 {
     internal class Item
     {
+        private int UpId {  get; set; }
+        private int IdItem {  get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private double Price { get; set; }
+        private string Color { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private Category Category { get; set; }
+        private int Stock {  get; set; }
+        private Boolean Availability { get; set; }
+        private DateTime RegistrationDate {  get; set; }
+        //private Warranty Warranty { get; set; }
+        //private List<Reviews> Reviews { get; set; } 
+        //images?
+        //private TechnicalSpecifications TechnicalSpecifications { get; set; } 
+        //private Dimensions Dimensions { get; set; }
+        private double Weight { get; set; }
+
+        public Item(){}
+
+        public Item(string name, string description, double price,
+                    int stock, bool availability, DateTime registrationDate)
+        {
+            IdItem = 1 + UpId++;
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Availability = availability;
+            RegistrationDate = registrationDate;
+        }
+
+        public override string ToString()
+        {
+            return "[ID do Produto:" + IdItem 
+                + " | Nome do Produto: " + Name 
+                + " | Preco do Produto: " + Price 
+                + " | Estoque do Produto: " + Stock 
+                + " | Data de entrada do Produto: " + RegistrationDate + "]";
+        }
     }
 }
+

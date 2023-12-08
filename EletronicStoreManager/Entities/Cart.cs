369731dc2e1d74c0294a8e41900cf13251ba6ff1
Ruan <domruan333@gmail.com>
2023-12-08 15:12:23 -0300

    Implementado carrinho de compras

--- EletronicStoreManager/Entities/Cart.cs
+++ EletronicStoreManager/Entities/Cart.cs
@@ -8,5 +8,31 @@ namespace EletronicStoreManager.Entities
 {
     internal class Cart
     {
+        public static int UpId = 1;
+        public int CartId { get; private set; }
+        public int CustomerId { get; private set; }
+        public DateTime CreationDate {  get; private set; }
+        public List<ItemsCart> Items { get; private set; }
+        public double TotalAmount { get; private set; }
+
+        public Cart(Customer customerId, DateTime creationDate, 
+            List<ItemsCart> items)
+        {
+            CartId = UpId++;
+            CustomerId = customerId.CustomerId;
+            CreationDate = creationDate;
+            Items = items ?? new List<ItemsCart>();
+            Calcular();
+        }
+
+        public void Calcular()
+        {   
+            TotalAmount = 0;
+            foreach( var item in Items)
+            {
+                TotalAmount += item.TotalPrice;
+                
+            }
+        } 
     }
 }
